79d57b555816f3ef4f5a900d060eb0df77df3641
Alexandre Hastings <alex.hastings.1227@gmail.com>
2023-09-22 02:14:18 -0600

    add basic js functionality

--- main.js
+++ main.js
@@ -1,21 +1,110 @@
-var startButton = document.querySelector("#start-button");
+var frogEomji = "🐸"
+var crabEmoji = "🦀"
 
-function hideElement(elementId){
-    document.querySelector(elementId).classList.add("hidden")
+var currentPlayer = frogEomji
+var gameBoard = ["","","","","","","","",""]
+var gameActive = true
+
+function clickButton(buttonIndex) {
+    if (gameActive && gameBoard[buttonIndex] === "") {
+        gameBoard[buttonIndex] = currentPlayer
+        document.getElementById(`button${buttonIndex + 1}`).textContent = currentPlayer
+
+        if (checkWin()) {
+            announceResult(`${currentPlayer} wins!`)
+            updatePlayerWins(currentPlayer)
+            gameActive = false
+            autoResetGame()
+        } else if (gameBoard.includes("")) {
+            currentPlayer = currentPlayer === frogEomji ? crabEmoji : frogEomji
+            updatePlayerTurn()
+        } else {
+            announceResult("Draw!!!!")
+            gameActive = false
+            autoResetGame()
+        }
+    }
 }
-function showElement(elementId){
-    document.querySelector(elementId).classList.remove("hidden")
+
+function checkWin() {
+    var winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    for (var i = 0; i < winPatterns.length; i++) {
+        var pattern = winPatterns[i];
+        var a = pattern[0];
+        var b = pattern[1];
+        var c = pattern[2];
+        if (gameBoard[a] !== '' && gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c]) {
+            return true;
+        }
+    }
+    return false;
 }
 
-startButton.addEventListener("click", function(event) {
-    var player1Name = document.querySelector("#player1").value 
-    var player2Name = document.querySelector("#player2").value 
-    event.preventDefault()
+function announceResult(result) {
+    document.getElementById("current-player-name").textContent = result
+}
 
-    if (player1Name === "" || player2Name === "") {
-        alert("Both player names are required!");
+function updatePlayerTurn() {
+    document.getElementById("current-player-name").textContent = `Current Player Turn is: ${currentPlayer} `
+}
+
+function updatePlayerWins(player) {
+    if (player === frogEomji) {
+        var player1Wins = parseInt(document.getElementById('player1-wins').textContent) + 1
+        document.getElementById('player1-wins').textContent = player1Wins
+    } else if (player === crabEmoji) {
+        var player2Wins = parseInt(document.getElementById("player2-wins").textContent) + 1
+        document.getElementById("player2-wins").textContent = player2Wins
+    }
+}
+
+const buttons = document.querySelectorAll('.cell');
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', (function(index) {
+        return function() {
+            clickButton(index);
+        };
+    })(i));
+}
+function resetGame() {
+    currentPlayer = frogEomji
+    gameBoard = ["","","","","","","","",""]
+    gameActive = true
+
+    var buttons = document.querySelectorAll(".cell")
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].textContent = ""
+    }
+    updatePlayerTurn()
+    announceResult("")
+}
+
+function autoResetGame() {
+    setTimeout(function() {
+        resetGame();
+    }, 5000); // 5000 milliseconds = 5 seconds
+}
+
+document.getElementById('start-button').addEventListener('click', (event) => {
+    event.preventDefault();
+    const player1Name = document.getElementById('player1').value;
+    const player2Name = document.getElementById('player2').value;
+
+    if (player1Name && player2Name) {
+        document.getElementById('player1-name').textContent = player1Name;
+        document.getElementById('player2-name').textContent = player2Name;
+
+        // Show player information and hide the setup form
+        document.getElementById('player-info').classList.remove('hidden');
+        document.getElementById('front-page').classList.add('hidden');
+        document.getElementById('game-board').classList.remove('hidden');
+        resetGame();
     } else {
-        hideElement("#front-page");
-        showElement("#game-board");
+        alert('Please enter names for both players.');
     }
 });
