978022c7daa8880bf023f9a57924dbbbefe2dd57
Alexandre Hastings <alex.hastings.1227@gmail.com>
2023-09-22 15:00:11 -0600

    add start game limit function and picture

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ var gameBoard = ["","","","","","","","",""]
 var gameActive = true
 
 function clickButton(buttonIndex) {
-    if (gameActive && gameBoard[buttonIndex] === "") {
+    if (!gameActive || !playerNamesFilled() || gameBoard[buttonIndex] === !"") {
+        return;
+    }
         gameBoard[buttonIndex] = currentPlayer
         document.getElementById(`button${buttonIndex + 1}`).textContent = currentPlayer
 
@@ -24,7 +26,7 @@ function clickButton(buttonIndex) {
             autoResetGame()
         }
     }
-}
+
 
 function checkWin() {
     var winPatterns = [
@@ -92,13 +94,14 @@ function autoResetGame() {
 
 document.getElementById('start-button').addEventListener('click', (event) => {
     event.preventDefault();
-    const player1Name = document.getElementById('player1').value;
-    const player2Name = document.getElementById('player2').value;
 
-    if (player1Name && player2Name) {
-        document.getElementById('player1-name').textContent = player1Name;
-        document.getElementById('player2-name').textContent = player2Name;
+    if (playerNamesFilled()) {
 
+        var player1Name = document.getElementById('player1').value;
+        var player2Name = document.getElementById('player2').value;
+
+        document.getElementById("player1-name").textContent = player1Name
+        document.getElementById("player2-name").textContent = player2Name
         // Show player information and hide the setup form
         document.getElementById('player-info').classList.remove('hidden');
         document.getElementById('front-page').classList.add('hidden');
@@ -108,3 +111,13 @@ document.getElementById('start-button').addEventListener('click', (event) => {
         alert('Please enter names for both players.');
     }
 });
+
+function playerNamesFilled(){
+    var player1Name = document.getElementById('player1').value;
+    var player2Name = document.getElementById('player2').value;
+
+    if (!player1Name || !player2Name){
+        return false
+    }
+    return true
+}
