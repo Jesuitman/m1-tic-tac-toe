1c86b0a01765ce28c967a2e1928aa977108ab7d5
Alexandre Hastings <alex.hastings.1227@gmail.com>
2023-09-22 22:43:43 -0600

    added in functionality for Nightmare mode and for Chaos mode

--- main.js
+++ main.js
@@ -88,3 +88,100 @@ function playerNamesFilled(){
     }
     return true
 }
+
+//Nightmare mode! DO NOT USE IF EPILEPTIC
+
+// var systemFonts = [
+// 'American Typewriter',
+// 'Andale Mono',
+// 'Arial',
+// 'Arial Black',
+// 'Arial Narrow',
+// 'Arial Rounded MT Bold',
+// 'Arial Unicode MS',
+// 'Avenir',
+// 'Avenir Next',
+// 'Avenir Next Condensed',
+// 'Baskerville',
+// 'Big Caslon',
+// 'Bodoni 72',
+// 'Bodoni 72 Oldstyle',
+// 'Bodoni 72 Smallcaps',
+// 'Bradley Hand',
+// 'Brush Script MT',
+// 'Chalkboard',
+// 'Chalkboard SE',
+// 'Chalkduster',
+// 'Charter',
+// 'Cochin',
+// 'Comic Sans MS',
+// 'Copperplate',
+// 'Courier',
+// 'Courier New',
+// 'Didot',
+// 'DIN Alternate',
+// 'DIN Condensed',
+// 'Futura',
+// 'Geneva',
+// 'Georgia',
+// 'Gill Sans',
+// 'Helvetica',
+// 'Helvetica Neue',
+// 'Herculanum',
+// 'Hoefler Text',
+// 'Impact',
+// 'Lucida Grande',
+// 'Luminari',
+// 'Marker Felt',
+// 'Menlo',
+// 'Microsoft Sans Serif',
+// 'Monaco',
+// 'Noteworthy',
+// 'Optima',
+// 'Palatino',
+// 'Papyrus',
+// 'Phosphate',
+// 'Rockwell',
+// 'Savoye LET',
+// 'SignPainter',
+// 'Skia',
+// 'Snell Roundhand',
+// 'Tahoma',
+// 'Times',
+// 'Times New Roman',
+// 'Trattatello',
+// 'Trebuchet MS',
+// 'Verdana',
+// 'Zapfino',
+// ]
+
+// function randomizeFont() {
+//     document.body.style.fontFamily = "serif"
+//     const fontIndex = Math.floor(Math.random() * systemFonts.length);
+//     const randomFont = systemFonts[fontIndex];
+
+//     const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16)
+
+//     document.body.style.fontFamily = randomFont;
+//     document.head.style.fontFamily = randomFont
+//     document.body.style.color = randomColor
+//     document.head.style.color = randomColor
+// }
+// setInterval(randomizeFont, 50)
+
+//Chaos Mode! Dont try this! Unless told to
+
+// function randomizeElementSizes() {
+//     const elements = document.querySelectorAll('*'); // Select all elements on the page
+
+//     elements.forEach((element) => {
+//         // Generate a random size between 10 and 100 pixels (you can adjust the range)
+//         const randomSize = Math.floor(Math.random() * 91) + 10;
+
+//         // Set the width and height of the element to the random size
+//         element.style.width = randomSize + 'px';
+//         element.style.height = randomSize + 'px';
+//     });
+// }
+
+// setInterval(randomizeElementSizes,50)
