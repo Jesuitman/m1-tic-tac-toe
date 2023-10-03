e2279d32299e966bfd0cf6e706dcf574b4341cdf
Alexandre Hastings <alex.hastings.1227@gmail.com>
2023-10-03 09:09:18 -0400

    simplified functions with array methods

--- main.js
+++ main.js
@@ -7,31 +7,24 @@ var gameActive = true
 
 var buttons = document.querySelectorAll('.cell')
 
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', (function(index) {
-        return function() {
-            clickButton(index)
-        }
-    })(i))
-}
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      clickButton(index);
+    });
+});
 
 function checkWin() {
-    var winPatterns = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ]
-    for (var i = 0; i < winPatterns.length; i++) {
-        var pattern = winPatterns[i]
-        var a = pattern[0]
-        var b = pattern[1]
-        var c = pattern[2]
-        if (gameBoard[a] !== '' && gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c]) {
-            return true
-        }
-    }
-    return false
-}
+    const winPatterns = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
+  
+    return winPatterns.some(pattern => {
+      const [a, b, c] = pattern;
+      return gameBoard[a] !== '' && gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c];
+    });
+  }
 
 function clickButton(buttonIndex) {
     if (!gameActive || !playerNamesFilled() || gameBoard[buttonIndex] === !"") {
@@ -57,34 +50,23 @@ function clickButton(buttonIndex) {
 }
 
 function resetGame() {
-    if (currentPlayer === frogEomji){
-    currentPlayer = crabEmoji
-    } else {
-        currentPlayer = frogEomji
-    }
-    gameBoard = ["","","","","","","","",""]
-    gameActive = true
+    currentPlayer = currentPlayer === frogEomji ? crabEmoji : frogEomji;
+    gameBoard = ["", "", "", "", "", "", "", "", ""];
+    gameActive = true;
+  
+    document.querySelectorAll(".cell").forEach(button => button.textContent = "");
     
-    var buttons = document.querySelectorAll(".cell")
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].textContent = ""
-    }
-    updatePlayerTurn()
-    announceResult("")
+    updatePlayerTurn();
+    announceResult("");
 }
 
 function autoResetGame() {
-    setTimeout(function() {
-        resetGame()
-    }, 2000)
-}
+    setTimeout(resetGame, 2000);
+  }
 
-function playerNamesFilled(){
-    var player1Name = document.getElementById('player1').value
-    var player2Name = document.getElementById('player2').value
-
-    if (!player1Name || !player2Name){
-        return false
-    }
-    return true
+function playerNamesFilled() {
+    const player1Name = document.getElementById('player1').value;
+    const player2Name = document.getElementById('player2').value;
+  
+    return !!player1Name && !!player2Name;
 }
